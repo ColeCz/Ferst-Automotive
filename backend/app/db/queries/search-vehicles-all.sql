dac8d16eb5f9a923cf2b10027b09424ecddb1353
Czekala, John Coleman <jczekala3@gatech.edu>
2024-11-21 18:56:49 -0500

    updated search_vehicles() queries to be psycopg friendly

--- backend/app/db/queries/search-vehicles-all.sql
+++ backend/app/db/queries/search-vehicles-all.sql
@@ -1,19 +1,20 @@
-SELECT DISTINCT v.vin, v.vehicle_type, v.model_name, v.model_year, v.manufacturer, v.fuel_type, v.horsepower, v.description, STRING_AGG(c.color_name, ', ') AS colors, t.trans_id, 
-CASE
-		WHEN t.trans_id IS NOT NULL THEN 'Sold'
-		ELSE 'Available'
-END AS sale_status, 
-t.trans_date, t.trans_price
+SELECT DISTINCT v.vin, v.vehicle_type, v.model_name, v.model_year, v.manufacturer, v.fuel_type, v.horsepower, v.description, 
+       STRING_AGG(c.color_name, ', ') AS colors, t.trans_id, 
+       CASE
+           WHEN t.trans_id IS NOT NULL THEN 'Sold'
+           ELSE 'Available'
+       END AS sale_status, 
+       t.trans_date, t.trans_price
 FROM Vehicle v
 LEFT JOIN Color c ON v.vin = c.vin
 LEFT JOIN Transaction t ON v.vin = t.vehicle_vin
 WHERE 1=1
-AND (%(vehicle_type)s IS NULL OR v.vehicle_type = %(vehicle_type)s)
-AND (%(manufacturer)s IS NULL OR v.manufacturer = %(manufacturer)s)
-AND (%(year)s IS NULL OR v.model_year = %(year)s)
-AND (%(fuel_type)s IS NULL OR v.fuel_type = %(fuel_type)s)
-AND (%(vin)s IS NULL OR v.vin = %(vin)s)
-AND (%(description)s IS NULL OR v.description ILIKE '%' || %(description)s || '%') --TODO: is this right?
+AND (COALESCE(%(vehicle_type)s, '') = '' OR v.vehicle_type = %(vehicle_type)s)
+AND (COALESCE(%(manufacturer)s, '') = '' OR v.manufacturer = %(manufacturer)s)
+AND (COALESCE(%(year)s, '') = '' OR v.model_year = %(year)s)
+AND (COALESCE(%(fuel_type)s, '') = '' OR v.fuel_type = %(fuel_type)s)
+AND (COALESCE(%(vin)s, '') = '' OR v.vin = %(vin)s)
+AND (COALESCE(%(input)s, '') = '' OR v.description ILIKE %(input)s)     -- changed to match parameter name in other file
 GROUP BY v.vin, v.vehicle_type, v.model_name, v.model_year, 
          v.manufacturer, v.fuel_type, v.horsepower, v.description,
          t.trans_id, t.trans_date, t.trans_price;
