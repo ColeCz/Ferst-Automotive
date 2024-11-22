b3e466396c8f9a9f0b1e9afe9c8ae32085cce6ef
Coleman Czekala <jczekala3@gatech.edu>
2024-11-22 03:21:53 -0500

    added color search, added sorting

--- backend/app/db/queries/search-vehicles-unsold.sql
+++ backend/app/db/queries/search-vehicles-unsold.sql
@@ -9,6 +9,12 @@ AND (COALESCE(%(vehicle_type)s, '') = '' OR v.vehicle_type = %(vehicle_type)s)
 AND (COALESCE(%(manufacturer)s, '') = '' OR v.manufacturer = %(manufacturer)s)
 AND (COALESCE(%(year)s, '') = '' OR v.model_year = %(year)s)
 AND (COALESCE(%(fuel_type)s, '') = '' OR v.fuel_type = %(fuel_type)s)
-AND (COALESCE(%(input)s, '') = '' OR v.description ILIKE %(input)s)
+AND (COALESCE(%(keyword)s, '') = '' OR v.description ILIKE %(keyword)s)
+AND (COALESCE(%(color)s, '') = '' OR EXISTS (
+    SELECT 1
+    FROM Color c_sub
+    WHERE c_sub.vin = v.vin AND c_sub.color_name = %(color)s
+))
 AND t.trans_id IS NULL -- Only get unsold vehicles
-GROUP BY v.vin, v.vehicle_type, v.model_name, v.model_year, v.manufacturer, v.fuel_type, v.horsepower, v.description;
+GROUP BY v.vin, v.vehicle_type, v.model_name, v.model_year, v.manufacturer, v.fuel_type, v.horsepower, v.description
+ORDER BY v.vin ASC;
--- backend/app/db/queries/search-vehicles.sql
+++ backend/app/db/queries/search-vehicles.sql
@@ -1,30 +1,31 @@
-SELECT DISTINCT v.vin, v.vehicle_type, v.model_name, v.model_year,
-       v.manufacturer, v.fuel_type, v.horsepower, v.description,
-       STRING_AGG(c.color_name, ', ') as colors
+SELECT DISTINCT v.vin, v.vehicle_type, v.model_name, v.model_year, v.manufacturer, v.fuel_type, v.horsepower, v.description, STRING_AGG(c.color_name, ', ') AS colors
 FROM Vehicle v
-LEFT JOIN Color c ON v.vin = c.vin
+LEFT JOIN Color c 
+    ON v.vin = c.vin
 LEFT JOIN (
     SELECT t.vehicle_vin
     FROM Transaction t
-    JOIN Sale s ON t.trans_id = s.transactions
-) sold ON v.vin = sold.vehicle_vin
+    JOIN Sale s 
+        ON t.trans_id = s.transactions
+) sold 
+    ON v.vin = sold.vehicle_vin
 LEFT JOIN (
     SELECT vehicle_vin
     FROM Part
     WHERE current_status IN ('ORDERED', 'RECEIVED')
-) pending_parts ON v.vin = pending_parts.vehicle_vin
+) pending_parts 
+    ON v.vin = pending_parts.vehicle_vin
 WHERE sold.vehicle_vin IS NULL
 AND pending_parts.vehicle_vin IS NULL
+AND (COALESCE(%(vin)s, '') = '' OR v.vin = %(vin)s)
 AND (COALESCE(%(vehicle_type)s, '') = '' OR v.vehicle_type = %(vehicle_type)s)
 AND (COALESCE(%(manufacturer)s, '') = '' OR v.manufacturer = %(manufacturer)s)
 AND (COALESCE(%(year)s, '') = '' OR v.model_year = %(year)s)
 AND (COALESCE(%(fuel_type)s, '') = '' OR v.fuel_type = %(fuel_type)s)
+AND (COALESCE(%(keyword)s, '') = '' OR v.description ILIKE %(keyword)s)
 AND (COALESCE(%(color)s, '') = '' OR EXISTS (
-    SELECT 1 FROM Color
-    WHERE vin = v.vin AND color_name = COALESCE(%(color)s, color_name)
-))
-AND (COALESCE(%(input)s, '') = '' OR
-    v.description ILIKE %(input)s OR
-    v.model_name ILIKE %(input)s)
-GROUP BY v.vin, v.vehicle_type, v.model_name, v.model_year,
-         v.manufacturer, v.fuel_type, v.horsepower, v.description;
+    SELECT 1 FROM Color c_sub
+    WHERE c_sub.vin = v.vin AND c_sub.color_name = %(color)s
+    ))
+GROUP BY v.vin, v.vehicle_type, v.model_name, v.model_year, v.manufacturer, v.fuel_type, v.horsepower, v.description
+ORDER BY v.vin ASC;
