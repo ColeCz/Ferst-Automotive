d00825bbfbb7689e087abe54fdd99ef901499be9
dlin354 <dlin354@gatech.edu>
2024-11-23 20:51:25 -0500

    Added buyer contact info

--- backend/app/db/queries/get-vehicle-details-manager.sql
+++ backend/app/db/queries/get-vehicle-details-manager.sql
@@ -9,7 +9,10 @@ SELECT v.vin, v.vehicle_type, v.model_name, v.model_year, v.manufacturer, v.fuel
     st.trans_date AS sale_date, st.trans_price AS sale_price, s.salesperson AS selling_salesperson,
     -- Seller's contact info
     c.email AS seller_email, c.phone_num AS seller_phone, c.postal_code AS seller_postal_code,
-    c.state_abbrv AS seller_state, c.city AS seller_city, c.street AS seller_street
+    c.state_abbrv AS seller_state, c.city AS seller_city, c.street AS seller_street,
+    -- Buyer's contact info (only if vehicle has been sold)
+    bc.email AS buyer_email, bc.phone_num AS buyer_phone, bc.postal_code AS buyer_postal_code,
+    bc.state_abbrv AS buyer_state, bc.city AS buyer_city, bc.street AS buyer_street
 FROM Vehicle v
 LEFT JOIN transaction pt ON v.vin = pt.vehicle_vin
 LEFT JOIN Purchase p ON pt.trans_id = p.transactions
@@ -18,7 +21,9 @@ LEFT JOIN PartOrder po ON v.vin = po.vehicle_vin
 LEFT JOIN Part part ON po.vehicle_vin = part.vehicle_vin AND po.order_number = part.order_number
 LEFT JOIN transaction st ON v.vin = st.vehicle_vin
 LEFT JOIN Sale s ON st.trans_id = s.transactions
-LEFT JOIN Customer c ON pt.customer = c.customer_id -- Join to get the seller info
+LEFT JOIN Customer c ON pt.customer = c.customer_id -- Seller's contact info
+LEFT JOIN Customer bc ON st.customer = bc.customer_id -- Buyer's contact info (only if sold)
 WHERE v.vin = %(vin)s
-GROUP BY v.vin, pt.trans_date, pt.trans_price, p.condition, pc.username, part.part_number, part.part_description, part.current_status, part.unit_price, part.quantity, po.vendor, st.trans_date, st.trans_price, s.salesperson, c.email, c.phone_num, c.postal_code, c.state_abbrv, c.city, c.street;
+GROUP BY v.vin, pt.trans_date, pt.trans_price, p.condition, pc.username, part.part_number, part.part_description, part.current_status, part.unit_price, part.quantity, po.vendor, st.trans_date, st.trans_price, s.salesperson, c.email, c.phone_num, c.postal_code, c.state_abbrv, c.city, c.street, bc.email, bc.phone_num, bc.postal_code, bc.state_abbrv, bc.city, bc.street;
+
 
