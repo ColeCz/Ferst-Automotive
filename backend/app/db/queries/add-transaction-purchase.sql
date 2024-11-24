7c37c8b49e72454d360289b813e9e49e0da14ec3
Czekala, John Coleman <jczekala3@gatech.edu>
2024-11-24 00:12:14 -0500

    aligned parameter name with backend route

--- backend/app/db/queries/add-transaction-purchase.sql
+++ backend/app/db/queries/add-transaction-purchase.sql
@@ -1,6 +1,6 @@
 WITH new_transaction AS (
 	INSERT INTO Transaction (trans_date, trans_price, customer, vehicle_vin)
-	VALUES (CURRENT_TIMESTAMP, %(trans_price)s, %(customer)s, %(vehicle_vin)s)
+	VALUES (CURRENT_TIMESTAMP, %(trans_price)s, %(customer)s, %(vin)s)
 	RETURNING trans_id
 )
 INSERT INTO Purchase (transactions, clerk, condition)
